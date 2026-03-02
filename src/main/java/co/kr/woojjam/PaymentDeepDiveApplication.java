ef6abf55fc2d89a35c02f93531d6b0595c2afc1f
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-03-03 01:33:47 +0900

    init: 프로젝트 생성

---
+++ src/main/java/co/kr/woojjam/PaymentDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package co.kr.woojjam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentDeepDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentDeepDiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/kr/woojjam/payment_deep_dive/PaymentDeepDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.woojjam.payment_deep_dive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentDeepDiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
